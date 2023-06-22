4ff026bd04d891b96540d24b0572bf66bea923c6
_ <128780634+HatataMakuna@users.noreply.github.com>
2023-06-22 16:37:05 +0800

    added Practical 9

---
+++ practicals/Prac8/Prac8/03_DefaultMaster.master.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Net.Cache;
+
+public partial class DefaultMaster : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Retrieve Gender from Cookie 
+        //and display different advertisement banner based on gender
+        //place your code here...
+        string strGender = "";
+
+        if (Request.Cookies["Gender"] != null)
+        {
+            strGender = Request.Cookies["Gender"].Value;
+        }
+        if (strGender == "M")
+        {
+            Control ctrlAdvertisement;
+            ctrlAdvertisement = LoadControl("male.ascx");
+            contentAd.Controls.Add(ctrlAdvertisement);
+        }
+        else if (strGender == "F")
+        {
+            Control ctrlAdvertisement;
+            ctrlAdvertisement = LoadControl("female.ascx");
+            contentAd.Controls.Add(ctrlAdvertisement);
+        }
+        else
+        {
+            Control ctrlAdvertisement;
+            ctrlAdvertisement = LoadControl("general.ascx");
+            contentAd.Controls.Add(ctrlAdvertisement);
+        }
+
+    }
+    protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("Default.aspx");
+    }
+    protected void ImageButton2_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("Shop.aspx");
+    }
+    protected void ImageButton3_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Redirect("Contact.aspx");
+    }
+}
---
+++ practicals/Prac8/Prac8/Labels.ascx
@@ -0,0 +1,8 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Labels.ascx.cs" Inherits="Prac8.Labels" %>
+<div style="text-align:right; background-color:#BCDEFF">
+    <asp:Label ID="lblName" runat="server" ></asp:Label>
+    <br />
+    <asp:Label ID="lblTime" runat="server" ></asp:Label>
+    <br />
+    <asp:LoginStatus ID="LoginStatus1" runat="server" />
+</div>
